a427c83fe0adce43ca94769fce583ac078404017
Pankaj <patilpankaj9237@gmail.com>
2023-12-28 14:37:30 +0530

    Deployment

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "hero-pattern": "url('/assets/bac1.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
